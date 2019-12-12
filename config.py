cdef1cb45bdb7fa3f7bc1fbc6409f9596b5785b4
MansonLuo <1558911620@qq.com>
2019-12-12 17:43:47 +0800

    add unit test for password hash

--- config.py
+++ config.py
@@ -7,11 +7,11 @@ class Config:
     MAIL_PORT = int(os.environ.get('MAIL_PORT', '465'))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'false').lower() in ['true', 'on', '1']
     MAIL_USE_SSH = os.environ.get('MAIL_USE_SSH', 'true').lower() in ['true', 'on', '1']
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or '1558911620@qq.com'
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[FLASKY]'
     FLASKY_MAIL_SENDER = '1558911620@qq.com'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN') or '1558911620@qq.com'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     @staticmethod
