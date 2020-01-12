dfcd944c73ab464c324bd7ee58f5cc848d33d28f
mason <1558911620@qq.com>
2020-01-12 14:30:38 +0800

    Commplete chapter 14

--- flasky.py
+++ flasky.py
@@ -1,9 +1,16 @@
-import os
 from app import create_app, db
 from app.models import User, Role, Permission, Post, Follow, Comment
 from flask_migrate import Migrate
 from app.fake import users, posts, comments
+import os
+import sys
+import click
 
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+    COV = coverage.coverage(branch=True, include='app/*')
+    COV.start()
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
@@ -19,17 +26,30 @@ def test():
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
-# why i add this block of code, cause i foggote add lazy option  to relationship() method about followers relationship property in the User model
-"""     
-@app.context_processor
-def context_processer():
-    def my_len(list):
-        res = 0
-        for i in list:
-            res += 1
-        return res
-    return {'my_len': my_len}
-"""  
+
+@app.cli.command()
+@click.option('--coverage/--no-coverage', default=False, help='Run tests under code coverage')
+def test(coverage):
+    """Run the unit tests."""
+    if coverage and not os.environ.get('FLASK_COVERAGE'):
+        os.environ['FLASK_COVERAGE'] = '1'
+        os.execvp(sys.executable, [sys.executable] + sys.argv)
+
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+    if COV:
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'tmp/coverage')
+        COV.html_report(directory=covdir)
+        print('HTML vesion: file://%s/index.html' % covdir)
+
+        COV.erase()
 
 if '__main__' == __name__:
     app.run(debug=False, host="0.0.0.0") #ssl_context='adhoc')
